50dc56cdfe7aab64f97c6da307bd5976147367e6
haruto15z <ktc24a31f0004@edu.kyoto-tech.ac.jp>
2024-11-11 14:40:22 +0900

    バイクは全身で風を感じられるまで書いた

--- pages/example_bike.vue
+++ pages/example_bike.vue
@@ -4,9 +4,21 @@
 
 <template>
   <div class="container">
-    <p class="h1">あなたを虜にするバイクの魅力</p>
+    <p class="h1"><strong>あなたを虜にするバイクの魅力</strong></p>
+    
   </div>
  
 </template>
 
-<style></style>
+<style>h1 {
+  display: Flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+}
+
+h1{
+  text-align: center;
+}
+</style>
