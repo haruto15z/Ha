298a7537e85ca5d8ebbd38450131fb9239791790
haruto15z <ktc24a31f0004@edu.kyoto-tech.ac.jp>
2025-02-01 19:30:28 +0900

    画像を追加、サイト一つ追加

---
+++ pages/example_bike6.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="container">
+  <h1>原動機付自転車</h1>
+</div>
+<div class="container">
+ <p><strong>排気量</strong>50cc以下のエンジンを搭載しています。</p>
+ <p><strong>速度制限</strong>時速30～60km程度が一般的</p>
+ <p><strong>免許</strong>16歳以上で取得可能で、普通免許を所持していれば50cc以下の原動機付き自転車を運転できます。</p>
+ <p><strong>税金＆保険</strong>自動車税が安く、維持費が比較的少ないのが特徴です。</p>
+ <p>原動機付き自転車は、50cc以下のエンジンを搭載した二輪車で、軽便で手軽に移動手段として利用できるバイクです。</p>
+</div>
+</template>
